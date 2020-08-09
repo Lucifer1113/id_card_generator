af54747b1a3788aa30d3196d4c52ea19a060b6ea
Lucifer1113 <kmanish13.gkp@gmail.com>
2020-08-09 22:32:12 +0530

    changed few functionalities

--- README.md
+++ README.md
@@ -18,6 +18,15 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.<br />
 You will also see any lint errors in the console.
 
+## Snapshots
+
+<img src="https://ibb.co/vDKg0Td" alt="Snapshot-1"/>
+<img src="https://ibb.co/hMmq2cd" alt="Snapshot-2"/>
+<img src="https://ibb.co/PQC3Y7B" alt="Snapshot-3"/>
+<img src="https://ibb.co/1rHrtpB" alt="Snapshot-4"/>
+
+
 ## Preview
 
-<img src="https://s5.gifyu.com/images/fakeId.gif" alt="id-preview-gif"/>
+<img src="https://ibb.co/PCSXWZw" alt="id-preview-gif"/>
+
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {createRef,Component} from 'react';   // need to discuss
+import React, {Component} from 'react';   
 import Card from "./components/id-card/card";
 import './App.css';
 
@@ -20,12 +20,9 @@ class App extends Component {
       showInputNationality: false,
       themeColor: ['#14213d', '#3c2c3e', '#004D40', "#3E2723", "#212121", "#263238"]
     }
-    this.myInput = createRef();      
+          
   }
-  componentDidMount() {
-    const node = this.myInput.current;  // need to discuss
-    console.log(node)
-  }
+  
   handleRandomClick = () =>{
     fetch('https://api.randomuser.me/')
     .then(data=> data.json())
@@ -33,14 +30,15 @@ class App extends Component {
       console.log(response.results[0])
       let {name, email, dob, phone, nat : nationality, picture : image} = response.results[0];
       name = name['first'] + " " + name['last'];
-      dob = (new Date(dob['date'])).toLocaleDateString(); // need to discuss......
+      dob = (new Date(dob['date'])).toLocaleDateString(); 
       image = image['large'];
       this.setState({ name, email, dob, image, nationality, phone})
     })
+    .catch(err=>alert(err))
   }
   
   handleThemeClick = (theme) => {
-    const card = document.body.getElementsByClassName("id-card")[0];   // need to discuss
+    const card = document.getElementsByClassName("id-card")[0];   
     card.style.backgroundColor = theme;
   }
   handleClick = (type) => {
@@ -58,23 +56,23 @@ class App extends Component {
     }
   }
   handleChange = e =>{
-    const type = e.currentTarget.name;  //need to discuss
+    const type = e.target.name;  
     switch(type){
-      case 'name': this.setState({name: e.currentTarget.value});
+      case 'name': this.setState({name: e.target.value});
       break;
-      case 'email': this.setState({email: e.currentTarget.value})
+      case 'email': this.setState({email: e.target.value})
       break;
-      case 'dob': this.setState({dob: e.currentTarget.value})
+      case 'dob': this.setState({dob: e.target.value})
       break;
-      case 'phone': this.setState({phone: e.currentTarget.value})
+      case 'phone': this.setState({phone: e.target.value})
       break;
-      case 'nationality': this.setState({nationality: e.currentTarget.value})
+      case 'nationality': this.setState({nationality: e.target.value})
       break;
     }
   }
   handleKeyChange = e =>{
     if(e.key === "Enter"){
-      switch(e.currentTarget.name){
+      switch(e.target.name){
         case 'name': this.setState({showInputName: false});
         break;
         case 'email': this.setState({showInputEmail: false});
@@ -95,7 +93,6 @@ class App extends Component {
         <h1>ID CARD GENERATOR</h1>
 
         <Card 
-        ref={this.myInput}
         name={name} 
         email={email} 
         dob={dob} 
@@ -115,9 +112,7 @@ class App extends Component {
       <div className="df">
         {this.state.themeColor.map(theme=>(<div className="box m-2 circle" style={{backgroundColor: theme}} onClick={()=>this.handleThemeClick(theme)}></div>))}
       </div>
-    
         <button className="btn" onClick={this.handleRandomClick}>Random</button>
-      
       </div>
     );
   }
